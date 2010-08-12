e43ee26396be2a31731a0edacc60bced272458c9
Mao Yu <maoserr@gmail.com>
2010-08-12 15:01:47 -0400

    package only release files, fixed mingwrc error

--- CMakeModules/AddMingwRC.cmake
+++ CMakeModules/AddMingwRC.cmake
@@ -1,7 +1,8 @@
-# Function to add an RC file to a windows executeable file
-# The RC compiler defaults to windres, this can be overriden
-# after you've included the file.
-set(CMAKE_RC_COMPILER "windres")
+if (WIN32)
+	if(MINGW)
+		set(CMAKE_RC_COMPILER "windres")
+	endif(MINGW)
+endif(WIN32)
 
 # Parameters
 # - rc_path			Path to the rc file
@@ -10,16 +11,16 @@ set(CMAKE_RC_COMPILER "windres")
 macro(ADD_RC_FILE rc_path rc_file src_variable)
 if (WIN32)
 	if(MINGW)
-	# resource compilation for mingw
-	ADD_CUSTOM_COMMAND(OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/${rc_file}.o
-						COMMAND ${CMAKE_RC_COMPILER}
-							-i${rc_path}/${rc_file}.rc
-							-o ${CMAKE_CURRENT_BINARY_DIR}/${rc_file}.o)
-	set(${src_variable} ${${src_variable}}
-		${CMAKE_CURRENT_BINARY_DIR}/${rc_file}.o)
-  else(MINGW)
-	  set (${src_variable} ${${src_variable}}
-		  ${rc_path}/${rc_file})
+		# resource compilation for mingw
+		ADD_CUSTOM_COMMAND(OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/${rc_file}.o
+							COMMAND ${CMAKE_RC_COMPILER}
+								-i${rc_path}/${rc_file}.rc
+								-o ${CMAKE_CURRENT_BINARY_DIR}/${rc_file}.o)
+		set(${src_variable} ${${src_variable}}
+			${CMAKE_CURRENT_BINARY_DIR}/${rc_file}.o)
+	else(MINGW)
+		set (${src_variable} ${${src_variable}}
+			${rc_path}/${rc_file}.rc)
   endif(MINGW)
 endif (WIN32)
 endmacro(ADD_RC_FILE rc_path rc_file src_variable)
