e5f7ca8a41fddbc7180c34b5aaff70dde8ce8c8d
maoserr <maoserr@gmail.com>
2014-06-07 21:56:20 +0900

    Added resource files

---
+++ src/win32/resource.h
@@ -0,0 +1,20 @@
+#ifndef IDC_STATIC
+#define IDC_STATIC (-1)
+#endif
+
+#define IDI_ICON1                               1
+#define IDD_MAINDIALOG                          101
+#define IDD_OPTIONDIALOG                        103
+#define IDD_ABOUTDIALOG                         105
+#define IDC_MAIN_ST_INFO                        40000
+#define IDC_MAIN_CHK_AUTO                       40001
+#define IDC_MAIN_SL_AUTO                        40002
+#define IDC_MAIN_SL_BRI                         40003
+#define IDC_MAIN_ST_CURR                        40004
+#define IDC_MAIN_ST_DAY                         40005
+#define IDC_MAIN_ST_NIGHT                       40006
+#define IDC_MAIN_ST_LOC                         40007
+#define IDC_MAIN_BTN_ABOUT                      40010
+#define IDC_MAIN_BTN_PREV                       40011
+#define IDC_MAIN_BTN_OPT                        40012
+#define IDC_MAIN_BTN_HIDE                       40013
